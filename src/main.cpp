686dcbebc2265a578a9c9af69fc798163bbe91f7
Mrasch1 <mrasch1102@gmail.com>
2024-11-27 10:33:57 +0100

    esp32 infrared light with debounce

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,59 @@
 #include <Arduino.h>
 
-// put function declarations here:
-// int myFunction(int, int);
 
 #define SENSOR_PIN 33 // Define GPIO33 as the sensor pin
+#define DEBOUNCE_DELAY 50 // Debounce time in milliseconds
+
+
+// put function declarations here:
+// int myFunction(int, int);
+unsigned long lastDebounceTime = 0; // Last time the sensor state changed
+int lastStableState = LOW; // Last stbale state of the sensor
+int currentState; // Current state of the sensor
+
 
 void setup() {
   // put your setup code here, to run once:
   // int result = myFunction(2, 3);
   Serial.begin(9600);
   pinMode(SENSOR_PIN, INPUT);
+  Serial.println("Sensor monitoring with debounce...");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int sensorValue = digitalRead(SENSOR_PIN);
-  if (sensorValue == HIGH)
+  int reading = digitalRead(SENSOR_PIN); // Read the current state of the sensor
+  
+  // Check if the sensor state has changed
+  if (reading != lastStableState)
   {
-    Serial.println("Object detected!");
+    // Reset the debounce timer
+    lastDebounceTime = millis();
   }
-  else
+
+  // If the state has been stable for the debounce delay
+  if((millis() - lastDebounceTime) > DEBOUNCE_DELAY) 
   {
-    Serial.println("No object detected.");
+    //only update the current state if it has truly changed
+    if(reading != currentState)
+    {
+      currentState = reading;
+
+      // print the stable state
+      if (currentState == HIGH)
+      {
+       Serial.println("Object detected!");
+      }
+      else
+      {
+        Serial.println("No object detected.");
+      }
+      
+    }
   }
-  delay(100);
+
+  lastStableState = reading;
+  
 }
 
 // put function definitions here:
