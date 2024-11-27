16bac3a91f15fc888dcf708ece15d17bda6182b6
Mrasch1 <mrasch1102@gmail.com>
2024-11-27 11:02:18 +0100

    deep sleep

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,15 @@
 
 #define SENSOR_PIN 33 // Define GPIO33 as the sensor pin
 #define DEBOUNCE_DELAY 50 // Debounce time in milliseconds
-
+#define SLEEP_TIMEOUT 10000 // time (ms) before entering deep sleep if nothing is detected
+// 10 000 milliseconds is 10 seconds
 
 // put function declarations here:
 // int myFunction(int, int);
+unsigned long lastActivityTime = 0;
 unsigned long lastDebounceTime = 0; // Last time the sensor state changed
 int lastStableState = LOW; // Last stbale state of the sensor
-int currentState; // Current state of the sensor
+int currentState = LOW; // Current state of the sensor
 
 
 void setup() {
@@ -17,6 +19,7 @@ void setup() {
   // int result = myFunction(2, 3);
   Serial.begin(9600);
   pinMode(SENSOR_PIN, INPUT);
+  esp_sleep_enable_ext0_wakeup(GPIO_NUM_33, 1); // Set GPIO33 as wake-up source on HIGH signal
   Serial.println("Sensor monitoring with debounce...");
 }
 
@@ -43,6 +46,7 @@ void loop() {
       if (currentState == HIGH)
       {
        Serial.println("Object detected!");
+       lastActivityTime = millis(); // Reset the inactivity timer
       }
       else
       {
@@ -53,6 +57,14 @@ void loop() {
   }
 
   lastStableState = reading;
+
+  // Check for inactivity to enter deep sleep
+  if (millis() - lastActivityTime > SLEEP_TIMEOUT)
+  {
+    Serial.println("Entering deep sleep...");
+    delay(100); // Allow Serial message to be sent before sleep
+    esp_deep_sleep_start(); // Enter deep sleep
+  }
   
 }
 
